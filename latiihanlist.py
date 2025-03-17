be6e32f9599a651f4cbd40c5f8d0892d75c86bc2
Baiq Agestia Cahya Ilami <firtyafirmanagistia@gmail.com>
2025-03-17 23:23:59 +0800

    tugas list

---
+++ latiihanlist.py
@@ -0,0 +1,42 @@
+daftar_belanja = []
+
+def tambah_item(item):
+    daftar_belanja.append(item)
+    print(f'"{item}" telah ditambahkan ke daftar belanja.')
+def tampilkan_daftar():
+    if daftar_belanja:
+        print("\nDaftar Belanja:")
+        for i,item in enumerate(daftar_belanja, 1):
+            print(f"{i}.{item}")
+
+    else:
+        print("\nDaftar belanja kosong.")
+def hapus_item(index):
+    if 0 <= index < len(daftar_belanja):
+        item = daftar_belanja.pop(index)
+        print(f'"{item}"telah dihapus dari daftar belanja.')
+    else:
+        print("indeks tidak valid.")
+while True:
+    print("\nMenu:")
+    print("1.tambah item")
+    print("2.lihat daftar belanja")
+    print("3.hapus item item")
+    print("4. keluar")
+
+    pilihan = input("pilih menu (1-4):")
+
+    if pilihan == "1":
+        item = input("masukkan nama item: ")
+        tambah_item(item)
+    elif pilihan == "2":
+        tampilkan_daftar()
+    elif pilihan == "3":
+        tampilkan_daftar()
+        index = int(input("masukkan nomer item yang ingin dihapus: "))
+        hapus_item(index)
+    elif pilihan == "4":
+        print("terima kasih! program selesai.")
+    else:
+        print("pilihan tidak valid,silahkan coba lagi.")
+        
